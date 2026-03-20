5bfc3c84e5a07f2c6d593e194e6d818dac7b5ede
bhuvandow <117730566+bhuvandow@users.noreply.github.com>
2026-03-20 16:09:43 +0530

    auth-routes-fixed

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ console.log("App starting...");
 console.log("Connecting to DB...");
 
 mongoose
-  .connect("mongodb+srv://bhuvansharma1811:LP76NQoUzHLxVEiz@cluster0.xeshggt.mongodb.net/testDb?retryWrites=true&w=majority", {
+  .connect("mongodb+srv://bhuvansharma1811:LP76NQoUzHLxVEiz@cluster0.xeshggt.mongodb.net/testDbs", {
     serverSelectionTimeoutMS: 5000 // 👈 VERY IMPORTANT
   })
   .then(() => {
     console.log("✅ Database connected");
-    app.listen(3000, () => {
+    app.listen(3000, "0.0.0.0", () => {
       console.log("🚀 Server running on port 3000");
     });
   })
   .catch((err) => {
     console.error("❌ DB ERROR:", err);
   });
+
+
+  
